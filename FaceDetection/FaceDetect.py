85cd2df7a62ba0661e7e0a2d6f64fd34166d87fb
STkangyh <k22101149@g.seoultech.ac.kr>
2023-12-10 18:26:35 +0900

    Test

--- FaceDetection/FaceDetect.py
+++ FaceDetection/FaceDetect.py
@@ -9,17 +9,17 @@ def FaceDetect(img_path):
 
     if len(faces) > 0:
         for i, (x, y, w, h) in enumerate(faces):
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), 2)
             face = img[y:y+h, x:x+w]
             cv2.imwrite(img_path, face)  # 이미지 파일에 얼굴 덮어쓰기
 
 # FaceData 폴더 내에 있는 모든 폴더 가져오기
-folder_path = './FaceData/'
+folder_path = './FaceData2/'
 folder_list = os.listdir(folder_path)
 
 # 각 폴더 내의 이미지 파일 처리
 for folder in folder_list:
-    folder_path = os.path.join('./FaceData/', folder)
+    folder_path = os.path.join('./FaceData2/', folder)
     if os.path.isdir(folder_path):
         file_list = os.listdir(folder_path)
         for file in file_list:
